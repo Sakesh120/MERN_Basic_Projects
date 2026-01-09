6939af427cc1d91c1d1282f3131b0d229fb0f1ab
Sakesh120 <sakeshchopkar927@gmail.com>
2026-01-09 23:18:32 +0530

    backend

--- AI-ImageGenerator/backEnd/index.js
+++ AI-ImageGenerator/backEnd/index.js
@@ -1,6 +1,32 @@
 import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleroutes.js";
+
+dotenv.config();
+
 const app = express();
-app.get("/", (req, res) => {
-  res.send("helllo");
+app.use(cors());
+app.use(express.json());
+app.use("/api/vi/post", postRoutes);
+app.use("/api/vi/dalle", dalleRoutes);
+
+app.get("/", async (req, res) => {
+  res.send("Hello from DALL-E AI  API");
 });
-app.listen(3000);
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
